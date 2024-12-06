fc89b1f78c3e557ed5ac3dbac31eeedd61c94198
Meezaan-ud-Din A Wal-Ikram <m@meezaan.net>
2024-12-06 20:49:29 +0400

    Make country code nullable for countries like Kosovo

--- src/DTO/Geocode/Country.php
+++ src/DTO/Geocode/Country.php
@@ -5,7 +5,7 @@ namespace SevenEx\DTO\Geocode;
 final class Country
 {
     public function __construct(
-        readonly string $code,
+        readonly ?string $code,
         readonly string $name
     ) {}
 
